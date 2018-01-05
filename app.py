288b9917e4fbea9c2d3e3481ce0167ebfe4f82fd
Andriy <Andriyd909@gmail.com>
2018-01-05 15:01:11 +0200

    some more minor changes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask import request
+from flask import Flask, render_template, request, url_for, redirect, \
+    json, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
 
